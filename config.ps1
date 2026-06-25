6e51d3943572b699050dde2e6bfd91ce1def2d67
Sandeep Singh <131196655+SandeepSingh1991@users.noreply.github.com>
2026-06-25 11:18:27 +0530

    Add initial config.ps1

---
+++ config.ps1
@@ -0,0 +1,12 @@
+# Oracle A1 Free Toolkit Configuration
+$Region='ap-mumbai-1'
+$AvailabilityDomain='CnmG:AP-MUMBAI-1-AD-1'
+$CompartmentId='<COMPARTMENT_OCID>'
+$SubnetId='<SUBNET_OCID>'
+$ImageId='<IMAGE_OCID>'
+$Shape='VM.Standard.A1.Flex'
+$Ocpus=2
+$MemoryInGBs=12
+$BootVolumeSize=100
+$DisplayName='Oracle-A1-Free'
+$SshPublicKeyPath="$env:USERPROFILE\.ssh\id_ed25519.pub"
